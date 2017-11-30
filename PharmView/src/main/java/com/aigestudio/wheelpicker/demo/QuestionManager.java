cf6539b79c8dc02aa05f32cce94014fe065139c1
wilhsy86 <wilhsy86@yahoo.com>
2017-11-30 15:54:35 -0500

    ...

--- PharmView/src/main/java/com/aigestudio/wheelpicker/demo/QuestionManager.java
+++ PharmView/src/main/java/com/aigestudio/wheelpicker/demo/QuestionManager.java
@@ -1,37 +1,50 @@
 package com.aigestudio.wheelpicker.demo;
 
 import android.content.res.AssetManager;
-import android.view.View;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
-
-/**
- * Created by WiLhS on 11/30/2017.
- */
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class QuestionManager {
 
-//    AssetManager am = context.getAssets();
-//    InputStream is = am.open("test.txt");
-//
-//    ArrayList<String> arr = new ArrayList<String>();
-//        try (BufferedReader br = new BufferedReader(new FileReader())){
-//
-//        String sCurrentLine;
-//
-//        while ((sCurrentLine = br.readLine()) != null) {
-//            arr.add(sCurrentLine);
-//        }
-//
-//    } catch (IOException e) {
-//        e.printStackTrace();
-//    }
 
+    public static void main(String[] args) {
 
-    public QuestionManager() throws IOException {
+        String fileName = "Drugs.txt";
+        String[] colors = {"Red", "Lime", "Orange", "Brown", "Blue", "Grey",
+                "Yellow", "Green", "Pink", "Purple", "Maroon", "White", "Black"
+        };
+        List<String> colorList = Arrays.asList(colors);
+
+//        LinkedList<String> drugAttributes = new LinkedList<>();
+//        ArrayList<LinkedList> drugObjects = new ArrayList<>();
+        int ind = 0;
+        try {
+            File file = new File("@assets/drugs");
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter("\t|\r\n");
+
+            while (scanner.hasNextLine()) {
+                String input = scanner.next();
+                System.out.println(input);
+//                drugAttributes.add(input);
+
+                if (colorList.contains(input)) {
+                    System.out.println();
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '"
+                            + fileName + "'");
+        }
+
     }
+
 }
