8da63f4b6f549bfe14fddf9afac6f25a387c0f69
wilhsy86 <wilhsy86@yahoo.com>
2017-12-02 22:49:42 -0500

    ...

--- PharmView/src/main/java/com/aigestudio/wheelpicker/demo/QuestionManager.java
+++ PharmView/src/main/java/com/aigestudio/wheelpicker/demo/QuestionManager.java
@@ -1,66 +1,66 @@
-package com.aigestudio.wheelpicker.demo;
-
-
-import android.app.Activity;
-import android.app.Application;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
-import android.os.Environment;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-public class QuestionManager extends Activity{
-
-
-    public  QuestionManager()  throws IOException {
-
-
-
-
-
-
-
-
-    }
-
-
-//        String result;
+//package com.aigestudio.wheelpicker.demo;
 //
 //
-//        String fileName = "Drugs.txt";
-//        String[] colors = {"Red", "Lime", "Orange", "Brown", "Blue", "Grey",
-//                "Yellow", "Green", "Pink", "Purple", "Maroon", "White", "Black"
-//        };
-//        List<String> colorList = Arrays.asList(colors);
+//import android.app.Activity;
+//import android.app.Application;
+//import android.content.res.AssetManager;
+//import android.content.res.Resources;
+//import android.os.Environment;
 //
-////        LinkedList<String> drugAttributes = new LinkedList<>();
-////        ArrayList<LinkedList> drugObjects = new ArrayList<>();
-//        int ind = 0;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Scanner;
 //
-//        try {
-//            File file = new File("@assets/drugs.txt");
-//            Scanner scanner = new Scanner(file);
-//            scanner.useDelimiter("\t|\r\n");
-//            System.out.println("able to open file '" + fileName + "'");
+//public class QuestionManager extends Activity{
 //
-//            while (scanner.hasNextLine()) {
-//                String input = scanner.next();
-//                System.out.println(input);
-////                drugAttributes.add(input);
 //
-//                if (colorList.contains(input)) {
-//                    System.out.println();
-//                }
-//            }
-//        } catch (FileNotFoundException ex) {
-//            System.out.println("Unable to open file '" + fileName + "'");
-//        }
+//    public  QuestionManager()  throws IOException {
+//
+//
+//
+//
+//
+//
+//
+//
 //    }
-
-}
+//
+//
+////        String result;
+////
+////
+////        String fileName = "Drugs.txt";
+////        String[] colors = {"Red", "Lime", "Orange", "Brown", "Blue", "Grey",
+////                "Yellow", "Green", "Pink", "Purple", "Maroon", "White", "Black"
+////        };
+////        List<String> colorList = Arrays.asList(colors);
+////
+//////        LinkedList<String> drugAttributes = new LinkedList<>();
+//////        ArrayList<LinkedList> drugObjects = new ArrayList<>();
+////        int ind = 0;
+////
+////        try {
+////            File file = new File("@assets/drugs.txt");
+////            Scanner scanner = new Scanner(file);
+////            scanner.useDelimiter("\t|\r\n");
+////            System.out.println("able to open file '" + fileName + "'");
+////
+////            while (scanner.hasNextLine()) {
+////                String input = scanner.next();
+////                System.out.println(input);
+//////                drugAttributes.add(input);
+////
+////                if (colorList.contains(input)) {
+////                    System.out.println();
+////                }
+////            }
+////        } catch (FileNotFoundException ex) {
+////            System.out.println("Unable to open file '" + fileName + "'");
+////        }
+////    }
+//
+//}
